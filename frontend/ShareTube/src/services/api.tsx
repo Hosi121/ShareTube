fd282d2a507eefa165757ff19898c37398ba11dd
araaki12345 <0825araakimoro@gmail.com>
2024-07-07 22:20:24 +0900

    feat: ユーザー情報に関する設定を定義

--- frontend/ShareTube/src/services/api.tsx
+++ frontend/ShareTube/src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api", // バックエンドのURLに合わせて変更してください
+  baseURL: "http://localhost:8080/api",
   headers: {
     "Content-Type": "application/json",
   },
