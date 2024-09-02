5e77d11d0c33408b64709f832b1f0208c868a6bd
Hosi121 <takuyakataiwa@icloud.com>
2024-09-03 02:50:39 +0900

    fix : missing import

--- frontend/ShareTube/src/store/rootReducer.ts
+++ frontend/ShareTube/src/store/rootReducer.ts
@@ -1,5 +1,6 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import authReducer from './authSlice';
+import videoReducer from './videoSlice';
 
 // すべてのスライスを1つのrootReducerに結合
 const rootReducer = combineReducers({
